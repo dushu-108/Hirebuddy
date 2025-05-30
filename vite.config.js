9ed6e194713f7ca0db39e787510216c96e20ade6
Dushyanth Yalamanchili <167439018+dushu-108@users.noreply.github.com>
2025-05-30 14:06:34 +0530

    fix: update Vite config to bind to 0.0.0.0 for Render deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import process from 'node:process'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
+  server: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 5173,
+    strictPort: true
+  },
+  preview: {
+    port: process.env.PORT || 5173,
+    strictPort: true
+  }
 })
